1f6284d29581a4df6d1dd372dbbec96f23c0448f
tirthamoulib <btirthamouli@gmail.com>
2020-01-28 19:29:17 +0530

    Protoype and prototype inheritance

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 # Different design patterns that can be followed in vanilla js
 
-1. Module
+1. Module - IIFE: Immediately-Invoked-Function-Expressions
 2. Prototype
 3. Observer
 4. Singleton
---
+++ prototype/js/main.js
@@ -0,0 +1,40 @@
+(function () {
+    /**
+     * Constuctor function - creates a new animal
+     * @param {String} name 
+     */
+    function Animal(name) {
+        this.name = name
+    }
+    Animal.prototype.move = function () {
+        console.log(this.name, "is moving")
+    }
+
+    /**
+     * Cat extends animal
+     * @param {String} name 
+     * @param {String} breed 
+     */
+    function Cat(name, color) {
+        Animal.apply(this, [name])
+        this.color = color
+    }
+    Cat.prototype = Object.create(Animal.prototype)
+    Cat.prototype.speak = function () {
+        console.log(this.name, "says meow")
+    }
+
+    // Animal object
+    console.log("-----------------------ANIMAL-----------------------")
+    var grumpy = new Animal("Grumpy")
+    console.log(grumpy)
+    grumpy.move()
+
+    // Cat object
+    console.log("-------------------------CAT-------------------------")
+    var butterFingers = new Cat("Butter Fingers", "orange")
+    console.log(butterFingers)
+    butterFingers.move()
+    butterFingers.speak()
+})()
+
